7e9ff4ba430c0e701293faed5648024bb0f770a2
belisario-cogua <gad10cogua@gmail.com>
2021-06-01 14:48:17 -0500

    pruebas static

--- Apps/publicaciones/models.py
+++ Apps/publicaciones/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.utils import timezone
 from smartfields import fields
+from django.core.exceptions import ValidationError 
 # Create your models here.
 
 class Publicacion(models.Model):
+	def validate_image(fieldfile_obj): 
+		filesize = fieldfile_obj.file.size 
+		megabyte_limit = 1.0 
+		if filesize > megabyte_limit*1024*1024: 
+			raise ValidationError("El tamaño maximo de la imagen debe ser %sMB" % str(megabyte_limit))
+
 	nombre = models.CharField(max_length = 200)
 	descripcion = models.TextField('Descripción')
 	estado = models.BooleanField('Estado',default = True)
-	imagen = fields.ImageField('Imagen', upload_to='imagenes/publicaciones/%Y/%m/%d/', max_length=200, blank = True, null = True)
+	imagen = fields.ImageField('Imagen', upload_to='imagenes/publicaciones/%Y/%m/%d/', max_length=200, blank = True, null = True, validators=[validate_image])
 	created = models.DateTimeField('Fecha de publicación', editable=False)
 	modified = models.DateTimeField('Modificación', editable=False)
 

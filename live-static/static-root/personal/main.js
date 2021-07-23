a4bc2837ea669f3e8afe55c42c5ca922d50c280c
belisario-cogua <gad10cogua@gmail.com>
2021-07-23 14:21:46 -0500

    js y dominio arreglado

--- live-static/static-root/personal/main.js
+++ live-static/static-root/personal/main.js
@@ -82,7 +82,7 @@ function mostrarErroresEditar(errores){
 	    for (var i in errores.responseJSON.error[item]) {
 	      // object message error django
 	      var $input = $("input[name='"+ item +"']");
-	      $input.before('<div class = "alert alert-danger alert-dismissible fade show" ><button type="button" class="close" data-dismiss="alert">&times;</button><strong>Oops! </strong>' + errores.responseJSON.error[item][i] + '</div>');
+	      $input.before('<div class = "alert alert-danger alert-dismissible fade show" ><button type="button" class="close" data-dismiss="alert">&times;</button><strong></strong>' + errores.responseJSON.error[item][i] + '</div>');
 	    }
 		 
 	}

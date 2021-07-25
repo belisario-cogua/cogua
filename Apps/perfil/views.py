dc9e483d387463d038436adea41377f41311c662
belisario-cogua <gad10cogua@gmail.com>
2021-07-25 14:48:13 -0500

    email6

--- Apps/perfil/views.py
+++ Apps/perfil/views.py
@@ -65,19 +65,26 @@ class RegistrarUser(CreateView):
                 user = form.save(commit=False)
                 user.is_active = False
                 user.save()
+                print("formulario save............")
                 current_site = get_current_site(request)
+                print("formulario current_site.......")
                 mail_subject = 'Activa tu cuenta de COGUA'
+                print("formulario mail_subject...........")
                 message = render_to_string('perfil/acc_active_email.html', {
                     'user': user,
                     'domain': current_site.domain,
                     'uid':urlsafe_base64_encode(force_bytes(user.pk)),
                     'token':account_activation_token.make_token(user),
                 })
+                print("formulario message...........")
                 to_email = form.cleaned_data.get('email')
+                print("formulario to_email.............")
                 email = EmailMessage(
                             mail_subject, message, to=[to_email]
                 )
+                print("formulario email...........")
                 email.send()
+                print("formulario email send.........")
                 mensaje = f'{self.model.__name__} registrado correctamente!'
                 error = 'No hay error!'
                 response = JsonResponse({'mensaje':mensaje,'error':error,'url':self.success_url})
